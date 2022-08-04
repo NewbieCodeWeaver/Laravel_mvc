bb6fc279b8554acf99c4f78e9b0f97dd9bbdf14c
Newbiecodeweaver <carlosgilino@gmail.com>
2022-08-04 11:55:36 +0200

    index file name change for using blade

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+extends('layouts.plantilla')
+
+@section('title','Homepage')
+
+@section('content')
+  
+    <header>
+        <h1>Index</h1>      
+    </header>
+    Vista del index 
+    <footer>  
+    </footer>
+
+@endsection
