d40b9ca37f3bfdbf4d43457c41a663ec8d962556
Newbiecodeweaver <carlosgilino@gmail.com>
2022-08-05 09:45:53 +0200

    tailwind header styles

--- resources/views/layouts/includes/header.blade.php
+++ resources/views/layouts/includes/header.blade.php
@@ -1,6 +1,7 @@
-<div id="page">
+<div id="page" class=" w-full">
     <header>
-        <div id="header">
-            <div id="logo">logo</div>
-            <div id="nav">menu</div>     
+        <div id="header" class=" w-full bg-yellow-300 h-28 flex flex-row">
+            <div id="logo" class=" p-10">logo</div>
+            <div id="nav" class=" p-10">menu</div>
     </header>
+    </div>
