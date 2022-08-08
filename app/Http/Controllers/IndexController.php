1d561b1d3f4b508dc384f83086c09a105826d5f7
Newbiecodeweaver <carlosgilino@gmail.com>
2022-08-08 09:37:49 +0200

    listing partidos on homepage using IndexController

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\partido;
+
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
     public function __invoke() {
+
+        $partidos = partido::all();
+
+        return $partidos;
 
         return view('index');
 
