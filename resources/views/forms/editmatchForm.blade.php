8788e9041e82bdc7bdd69fd4720bf6f85ca8d13b
Newbiecodeweaver <carlosgilino@gmail.com>
2022-08-26 10:08:05 +0200

    edit form now shows match data

--- resources/views/forms/editmatchForm.blade.php
+++ resources/views/forms/editmatchForm.blade.php
@@ -6,37 +6,37 @@
         <label class="text-sm font-bold mb-3 block" for="equipo-local">
           Equipo local
         </label>
-        <input class="shadow  border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="equipo-local" type="text" value="" name="equipo_local">
+        <input class="shadow  border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="equipo-local" type="text" value="{{partido->equipo_local}}" name="equipo_local">
       </div>
       <div class="mb-4">
         <label class="text-sm font-bold mb-3 block" for="equipo-visitante">
           Equipo visitante
         </label>
-        <input class="shadow  border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="equipo-visitante" type="text" name="equipo_visitante">
+        <input class="shadow  border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="equipo-visitante" value="{{partido->equipo_visitante}}" type="text" name="equipo_visitante">
       </div>
       <div class="mb-4">
         <label class="text-sm font-bold mb-3 block" for="hora-partido">
            Hora
         </label>
-        <input class="shadow  border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="hora-partido" type="text" name="hora">
+        <input class="shadow  border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="hora-partido" value="{{partido->hora}}" type="text" name="hora">
       </div>
       <div class="mb-4">
         <label class="text-sm font-bold mb-3 block" for="fecha">
           Fecha
         </label>
-        <input class="shadow  border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="fecha-partido" type="text" name="fecha">
+        <input class="shadow  border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="fecha-partido" value="{{partido->fecha}}" type="text" name="fecha">
       </div>
       <div class="mb-4">
         <label class="text-sm font-bold mb-3 block" for="ubicacion-partido">
           Ubicacion
         </label>
-        <input class="shadow  border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="ubicacion-partido" type="text" name="ubicacion">
+        <input class="shadow  border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="ubicacion-partido" value="{{partido->ubicacion}}" type="text" name="ubicacion">
       </div>
       <div class="mb-4">
         <label class="text-sm font-bold mb-3 block" for="resultado-partido">
           Resultado
         </label>
-        <input class="shadow  border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="resultado-partido" type="text" name="resultado">
+        <input class="shadow  border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="resultado-partido" value="{{partido->resultado}}" type="text" name="resultado">
       </div>
       <button type="submit" class="bg-teal-500 hover:bg-teal-600 pt-3 pb-3 pl-5 pr-5 text-white rounded mr-3">Actualizar partido</button>
     </form>
