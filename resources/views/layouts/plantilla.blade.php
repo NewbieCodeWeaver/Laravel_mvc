851c5824af4205d778b379890e4f8d16ead771fe
Newbiecodeweaver <carlosgilino@gmail.com>
2022-08-29 18:46:24 +0200

    flowbite and font-awesome scripts added to head

--- resources/views/layouts/plantilla.blade.php
+++ resources/views/layouts/plantilla.blade.php
@@ -5,6 +5,9 @@
     <title>@yield('title')</title>
     <meta charset="UTF-8">
     <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" />
+    <script src="https://unpkg.com/flowbite@1.4.7/dist/datepicker.js"></script>
+
 
 <body>
     @yield('content')
