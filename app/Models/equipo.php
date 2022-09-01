3d890767c8c6b5b765b7abaa837f932b0f66163b
Newbiecodeweaver <carlosgilino@gmail.com>
2022-09-01 17:19:10 +0200

    model equipo changed for solving issue

--- app/Models/equipo.php
+++ app/Models/equipo.php
@@ -5,28 +5,24 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class partido extends Model
+class equipo extends Model
 {
-    use HasFactory;
+      // Relacion uno a muchos (inversa)
 
-
-    // Relacion uno a muchos (inversa)
-
-    public function club()
-    {
-
-        return $this->belongsTo('App\Models\Club');
-    }
-
-
-    
-    // Relacion uno a muchos
-
-    public function partidos()
-    {
-
-        return $this->hasMany('App\Models\Partidos');
-    }
-
-
+      public function club()
+      {
+  
+          return $this->belongsTo('App\Models\Club');
+      }
+  
+  
+      
+      // Relacion uno a muchos
+  
+      public function partidos()
+      {
+  
+          return $this->hasMany('App\Models\Partidos');
+      }
+  
 }
