e4b0880e7b1016e33c007d98f29cdbe413b32233
Newbiecodeweaver <carlosgilino@gmail.com>
2022-09-10 13:34:34 +0200

    forms updated

--- resources/views/forms/addmatchForm.blade.php
+++ resources/views/forms/addmatchForm.blade.php
@@ -1,4 +1,4 @@
-<div class ="p-6 mt-5  bg-teal-500"><h1 class="text-center text-xl">Nuevo partido</h1></div>
+<div class ="p-6 mt-5  bg-gray-600"><h1 class="text-center text-xl text-white">Nuevo partido</h1></div>
 <div class="bg-slate-200">
     <form class=" px-8 pt-6 pb-8 mb-4" method="POST">
     @csrf
@@ -6,41 +6,61 @@
         <label class="text-sm font-bold mb-3 block" for="equipo-local">
           Equipo local
         </label>
-      
-        <select id="equipo-local" name="equipo_local" class="shadow  border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+        <select id="equipo-local" name="equipo_local" class="shadow  border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline @error('equipo_local') is-invalid @enderror">
           @foreach($equipos as $equipo)
               <option value="{{ $equipo->id }}">{{ $equipo->nombre}}</option>
           @endforeach 
       </select>
+      @error('equipo_local')
+      <div class="alert alert-danger">{{ $message }}</div>
+  @enderror
       </div>
       <div class="mb-4">
         <label class="text-sm font-bold mb-3 block" for="equipo-visitante">
           Equipo visitante
         </label>
       
-        <select id="equipo-visitante" name="equipo_visitante" class="shadow  border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+        <select id="equipo-visitante" name="equipo_visitante" class="shadow  border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline @error('equipo_visitante') is-invalid @enderror">
           @foreach($equipos as $equipo)
-              <option value="{{ $equipo->id }}">{{ $equipo->nombre}}</option>
+          <option value="{{ $equipo->id }}">{{ $equipo->nombre}}</option>
           @endforeach 
       </select>
+      @error('equipo_visitante')
+      <div class="alert alert-danger">{{ $message }}</div>
+  @enderror
+
+  @error('equipo_visitante_division')
+  <div class="alert alert-danger">{{ $message }}</div>
+@enderror
+
       </div>
       <div class="mb-4">
         <label class="text-sm font-bold mb-3 block" for="hora-partido">
            Hora
         </label>
-        <input type="time" id="hora-partido" name="hora" class="shadow  border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+        <input type="time" id="hora-partido" name="hora" class="shadow  border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline @error('hora') is-invalid @enderror">
+        @error('hora')
+        <div class="alert alert-danger">{{ $message }}</div>
+    @enderror
       </div>
+
       <div class="mb-4">
         <label class="text-sm font-bold mb-3 block" for="fecha">
           Fecha
         </label>
-        <input datepicker datepicker-format="yyyy-mm-dd" class="shadow  border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="fecha-partido" type="text" name="fecha">
+        <input datepicker datepicker-format="yyyy-mm-dd" class="shadow  border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline @error('fecha') is-invalid @enderror" id="fecha-partido" type="text" name="fecha">
+        @error('fecha')
+        <div class="alert alert-danger">{{ $message }}</div>
+    @enderror
       </div>
       <div class="mb-4">
         <label class="text-sm font-bold mb-3 block" for="ubicacion-partido">
           Ubicacion
         </label>
-        <input class="shadow  border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="ubicacion-partido" type="text" name="ubicacion">
+        <input class="shadow  border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline @error('ubicacion') is-invalid @enderror" id="ubicacion-partido" type="text" name="ubicacion">
+        @error('ubicacion')
+        <div class="alert alert-danger">{{ $message }}</div>
+    @enderror
       </div>
       <div class="mb-4">
         <label class="text-sm font-bold mb-3 block" for="resultado-partido">
@@ -48,6 +68,6 @@
         </label>
         <input class="shadow  border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="resultado-partido" type="text" name="resultado">
       </div>
-      <button type="submit" class="bg-teal-500 hover:bg-teal-600 pt-3 pb-3 pl-5 pr-5 text-white rounded mr-3">Crear partido</button><a href="{{route('partido.index')}}"><input type="button" value="Volver" class="bg-blue-500 hover:bg-blue-600 pt-3 pb-3 pl-5 pr-5 text-white rounded">
+      <button type="submit" class="bg-red-600 hover:bg-red-800 pt-3 pb-3 pl-5 pr-5 text-white rounded mr-3">Crear partido</button><a href="{{route('partido.index')}}"><input type="button" value="Volver" class="bg-slate-700 hover:bg-slate-800 pt-3 pb-3 pl-5 pr-5 text-white rounded">
     </form>
   </div>
--- resources/views/forms/editmatchForm.blade.php
+++ resources/views/forms/editmatchForm.blade.php
@@ -1,4 +1,4 @@
-<div class ="p-6 mt-5  bg-teal-500"><h1 class="text-center text-xl">Editar partido</h1></div>
+<div class ="p-6 mt-5  bg-gray-600"><h1 class="text-center text-xl text-white">Editar partido</h1></div>
 <div class="bg-slate-200">
     <form action="{{route('partido.update', $partido)}}" class=" px-8 pt-6 pb-8 mb-4" method="POST">
     @csrf
@@ -8,13 +8,21 @@
         <label class="text-sm font-bold mb-3 block" for="equipo-local">
           Equipo local
         </label>
-        <input class="shadow  border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="equipo-local" type="text" value="{{$partido->equipo_local}}" name="equipo_local">
+        <select id="equipo-local" name="equipo_local" class="shadow  border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+          @foreach($equipos as $equipo)
+              <option @if($partido->Local === $equipo->nombre) selected @endif value="{{ $equipo->id }}">{{ $equipo->nombre}}</option>
+          @endforeach 
+      </select>
       </div>
       <div class="mb-4">
         <label class="text-sm font-bold mb-3 block" for="equipo-visitante">
           Equipo visitante
         </label>
-        <input class="shadow  border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="equipo-visitante" value="{{$partido->equipo_visitante}}" type="text" name="equipo_visitante">
+        <select id="equipo-visitante" name="equipo_visitante" class="shadow  border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+          @foreach($equipos as $equipo)
+              <option @if($partido->Visitante === $equipo->nombre) selected @endif value="{{ $equipo->id }}">{{ $equipo->nombre}}</option>
+          @endforeach 
+      </select>
       </div>
       <div class="mb-4">
         <label class="text-sm font-bold mb-3 block" for="hora-partido">
@@ -40,6 +48,6 @@
         </label>
         <input class="shadow  border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="resultado-partido" value="{{$partido->resultado}}" type="text" name="resultado">
       </div>
-      <button type="submit" class="bg-teal-500 hover:bg-teal-600 pt-3 pb-3 pl-5 pr-5 text-white rounded mr-3">Actualizar partido</button><a href="{{route('partido.index')}}"><input type="button" value="Volver" class="bg-blue-500 hover:bg-blue-600 pt-3 pb-3 pl-5 pr-5 text-white rounded">
+      <button type="submit" class="bg-red-600 hover:bg-red-800 pt-3 pb-3 pl-5 pr-5 text-white rounded mr-3">Actualizar partido</button><a href="{{route('partido.index')}}"><input type="button" value="Volver" class="bg-slate-700 hover:bg-slate-800 pt-3 pb-3 pl-5 pr-5 text-white rounded">
     </form>
   </div>
