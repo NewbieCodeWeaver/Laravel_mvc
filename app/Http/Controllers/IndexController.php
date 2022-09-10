351e9595508a24b705c3b5189a0061d34615d2ec
Newbiecodeweaver <carlosgilino@gmail.com>
2022-09-10 13:52:20 +0200

    index and match controllers updated

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -11,18 +11,7 @@ class IndexController extends Controller
     public function __invoke() {
 
 
-     // $partidos = partido::all();
-      
-
-     $partidos = partido::join('equipos as equipolocal', 'partidos.equipo_local', '=', 'equipolocal.id')
-     ->join('equipos as equipovisitante', 'partidos.equipo_visitante', '=', 'equipovisitante.id')
-     ->select('equipolocal.nombre as Local', 'equipovisitante.nombre as Visitante', 'partidos.hora', 'partidos.fecha', 'partidos.ubicacion', 'partidos.resultado', 'partidos.id')
-     ->get();
-
-
-    return view('index',compact('partidos'));
-
-     //return $partidos;
+    return view('index');
 
     }
 }
