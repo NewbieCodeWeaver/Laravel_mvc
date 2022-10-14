b427451bb7ef4ecefd867de4d9331705d4d846e8
Newbiecodeweaver <carlosgilino@gmail.com>
2022-10-14 10:41:24 +0200

    new functions created following mvc layered architecture

--- app/Models/partido.php
+++ app/Models/partido.php
@@ -18,5 +18,36 @@ class partido extends Model
         return $this->belongsTo('App\Models\Equipo');
     }
 
+
+    public function getMatchIndex() {
+
+
+        $partidos = partido::join('equipos as equipolocal', 'partidos.equipo_local', '=', 'equipolocal.id')
+        ->join('equipos as equipovisitante', 'partidos.equipo_visitante', '=', 'equipovisitante.id')
+        ->select('equipolocal.nombre as Local', 'equipovisitante.nombre as Visitante', 'partidos.hora', 'partidos.fecha', 'partidos.ubicacion', 'partidos.resultado', 'partidos.id')
+        ->paginate(5);
+
+
+        return $partidos;
+   
+
+    }
+
+
+    public function getPartido($partido) {
+
+
+        $partidos = partido::join('equipos as equipolocal', 'partidos.equipo_local', '=', 'equipolocal.id')
+        ->join('equipos as equipovisitante', 'partidos.equipo_visitante', '=', 'equipovisitante.id')
+        ->select('equipolocal.nombre as Local', 'equipovisitante.nombre as Visitante', 'partidos.hora', 'partidos.fecha', 'partidos.ubicacion', 'partidos.resultado', 'partidos.id')
+        ->where('partidos.id',"=",$partido)
+        ->get();
+
+
+        return $partidos;
+
+
+    }
+
     
 }
