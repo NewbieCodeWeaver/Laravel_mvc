20f6b6c17f9ac998cf2034e39843dfb55a29d282
Chally <carlosgilino@gmail.com>
2024-09-20 12:31:57 +0200

    extend indexController to show matches at homepage

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,15 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Models\partido;
-
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
-    public function __invoke() {
+    public function __invoke()
+    {
 
+        $modeloPartido = new partido();
+        $partidos = $modeloPartido->getMatchIndex();
 
-    return view('index');
+        $date = Carbon::now()->toDateString();
 
+        return view('index', compact('partidos', 'date'));
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,10 +6,10 @@
 <div class="hero bg-black h-screen overflow-hidden flex justify-center">
     <div class="inner-container pt-8 flex flex-col md:flex-row gap-y-10">
         <div class="flex flex-col gap-y-5 md:gap-y-12 justify-center items-center md:items-start md:w-2/3">
-            <h1 class="text-3xl md:text-5xl text-white text-center md:text-left pt-10 md:pt-0">
+            <h1 class="text-3xl 2xl:text-6xl md:text-xl text-white text-center md:text-left pt-10 md:pt-0">
                 Gestiona todos tus clubes</br> y partidos en un sólo sitio
             </h1>
-            <div class="justify-self-center lg:justify-self-start w-40 rounded-lg bg-red-600 p-4 text-center text-base font-semibold text-red-50 hover:bg-red-800">
+            <div class="justify-self-center lg:justify-self-start w-40 rounded-lg bg-primary p-4 text-center text-base font-semibold text-red-50 hover:bg-red-800">
                 <a href="{{route('partido.index')}}">
                     <span class="uppercase">
                         ver partidos
@@ -23,4 +23,57 @@
         </div>
     </div>
 </div>
+
+<!-- Proximos partidos -->
+
+<div class="home-content flex justify-center ">
+    <div class="inner-container">
+        <h2 class="text-5xl text-center my-16">Próximos partidos</h2>
+        <table class="w-full mb-16">
+            <thead class="border-b bg-secondary">
+                <tr>
+                    <th class="text-base font-medium text-white px-10 py-6 text-left">
+                        Equipo local
+                    </th>
+                    <th class="text-base font-medium text-white px-10 py-6 text-left">
+                        Equipo visitante
+                    </th>
+                    <th class="text-base font-medium text-white px-10 py-6 text-left">
+                        Hora
+                    </th>
+                    <th class="text-base font-medium text-white px-10 py-4 text-left">
+                        Fecha
+                    </th>
+                    <th class="text-base font-medium text-white px-10 py-4 text-left">
+                        Ubicación
+                    </th>
+                </tr>
+            </thead>
+
+            <tbody>
+                @foreach ($partidos as $partido)
+                <tr class=" bg-teal-50">
+                    <td class=" text-base text-gray-900 font-light px-10 py-4 whitespace-nowrap pb-5 ">
+                        {{$partido->Local}}
+                    </td>
+                    <td class="text-base text-gray-900 font-light px-10 py-4 whitespace-nowrap pb-5 ">
+                        {{$partido->Visitante}}
+                    </td>
+                    <td class="text-base text-gray-900 font-light px-10 py-4 whitespace-nowrap pb-5 ">
+                        {{$partido->hora}}
+                    </td>
+                    <td class="text-base text-gray-900 font-light px-10 py-4 whitespace-nowrap pb-5 ">
+                        {{$partido->fecha}}
+                    </td>
+                    <td class="text-base text-gray-900 font-light px-10 py-4 whitespace-nowrap pb-5 ">
+                        {{$partido->ubicacion}}
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
+
+
 @endsection
