8f8a6f0500eb19fc544374fc0e46b05a0be168cb
Chally <carlosgilino@gmail.com>
2024-11-05 13:14:19 +0100

    crete link to homepage on footer logo

--- resources/views/layouts/includes/footer.blade.php
+++ resources/views/layouts/includes/footer.blade.php
@@ -1,6 +1,6 @@
 <footer>
     <div id="footer-top" class="bg-gradient-to-b from-gray-700 to-gray-800 text-center p-5 text-white flex flex-col gap-y-8 md:gap-y-0 md:flex-row py-10">
-        <div class="md:w-1/3 flex flex-col items-center justify-center gap-3"><img src="{{url('images/logo.png');}}" width="180px" height="180px" alt="clubs manager" class="z-20 relative"><span>Clubs Manager <?php echo date('Y') ?></span></div>
+        <div class="md:w-1/3 flex flex-col items-center justify-center gap-3"><a href="{{route('index');}}"><img src="{{url('images/logo.png');}}" width="180px" height="180px" alt="clubs manager" class="z-20 relative"></a><span>Clubs Manager <?php echo date('Y') ?></span></div>
         <div class="md:w-1/3 flex flex-col items-center justify-center gap-1 md:gap-5">
             <h3 class="text-2xl">Menú</h3>
             <ul class="flex flex-col md:gap-3">
