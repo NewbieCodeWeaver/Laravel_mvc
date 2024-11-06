3a18184e80c68c7a1f2bbd13015aa22d5bf67300
Chally <carlosgilino@gmail.com>
2024-11-06 23:53:42 +0100

    fix model issues

--- app/Models/partido.php
+++ app/Models/partido.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -29,6 +28,7 @@ class Partido extends Model
             ->orderBy('fecha', 'desc')
             ->paginate(5);
 
+
         if ($partidos) return $partidos;
     }
 
@@ -52,33 +52,7 @@ class Partido extends Model
             ->where('partidos.id', '=', $partido)
             ->first();
 
+
         if ($partidos) return $partidos;
-    }
-
-    public function getPendingMatches()
-    {
-
-        $currentDate = Carbon::now()->format('d-m-y');
-        $currentTime = Carbon::now()->format('H:i:s');
-
-        $partidos = Partido::join('equipos as equipolocal', 'partidos.equipo_local', '=', 'equipolocal.id')
-            ->join('equipos as equipovisitante', 'partidos.equipo_visitante', '=', 'equipovisitante.id')
-            ->select('equipolocal.nombre as local', 'equipovisitante.nombre as visitante', 'partidos.*')
-            ->where(function ($query) use ($currentDate, $currentTime) {
-                $query->whereRaw("STR_TO_DATE(fecha, '%d-%m-%y') > STR_TO_DATE(?, '%d-%m-%y')", [$currentDate])
-                    ->orWhere(function ($query) use ($currentDate, $currentTime) {
-                        $query->whereRaw("STR_TO_DATE(fecha, '%d-%m-%y') = STR_TO_DATE(?, '%d-%m-%y')", [$currentDate])
-                            ->whereRaw("STR_TO_DATE(hora, '%H:%i') > STR_TO_DATE(?, '%H:%i')", [$currentTime]);
-                    });
-            });
-
-        if ($partidos) return $partidos->get();
-    }
-
-    public function countMatches()
-    {
-        $numeroPartidos = Partido::all()->count();
-
-        if ($numeroPartidos) return $numeroPartidos;
     }
 }
